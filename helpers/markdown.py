5cefdddbe9ed96948495a9ceaf6700bcb80d583a
Chris Duxbury <chris_duxbury@outlook.com>
2024-12-04 22:08:32 +0800

    add ini for section and category info

--- helpers/markdown.py
+++ helpers/markdown.py
@@ -1,10 +1,16 @@
-import os, json
+import os, json, configparser
 from markdownify import markdownify
 
 os.makedirs('markdown', exist_ok=True)
 
-with open(os.path.join('..', 'handoff_articles.json')) as json_data:
+with open('handoff_articles.json') as json_data:
         articles = json.load(json_data)
+        # print(articles)
+
+groupings = {
+        "sections": {},  # id -> name mapping
+        "categories": {},  # id -> name mapping
+}
 
 for article in articles:
         title = article['title'].lower()
@@ -14,7 +20,10 @@ for article in articles:
 
         markdown = markdownify(article['body'], heading_style="ATX")
 
-        with open(os.path.join('..', 'markdown', f'{title}.md'), 'w') as f:
+        groupings["sections"][article["section_id"]] = article["section_name"]
+        groupings["categories"][article["category_id"]] = article["category_name"]
+
+        with open(os.path.join('markdown', f'{title}.md'), 'w') as f:
                 f.write('---\n')
                 f.write(f'title: {article["title"]}\n')
                 f.write(f'id: {article["id"]}\n')
@@ -24,5 +33,18 @@ for article in articles:
                 f.write(f'category_name: {article["category_name"]}\n')
                 f.write('---\n')
                 f.write(markdown)
+
+# Create configparser instance
+config = configparser.ConfigParser()
+
+# Add sections to the config
+config['Sections'] = {str(article['section_id']): article['section_name'] for article in articles}
+config['Categories'] = {str(article['category_id']): article['category_name'] for article in articles}
+
+# Write to INI file
+with open('groupings.ini', 'w') as configfile:
+    config.write(configfile)
+
+print('Groupings saved to groupings.ini')
 
 print('HTML to Markdown conversion complete.')
